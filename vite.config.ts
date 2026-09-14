c90e7d87dd212292877ddc29a034fedd32643169
mmorenor1 <mmorenor@credicorpcapital.com>
2026-09-14 14:29:18 -0500

    Add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  base: './',
+})
